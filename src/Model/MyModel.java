85ebc395aa96b59817c92cf26c6000d39ecd0339
school & work <michalez@post.bgu.ac.il>
2019-06-04 14:43:14 +0300

    with mazeJAR

--- src/Model/MyModel.java
+++ src/Model/MyModel.java
@@ -1,26 +1,87 @@
 package Model;
 
+import algorithms.mazeGenerators.IMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
 import javafx.scene.input.KeyCode;
 
-public class MyModel implements IModel {
+import java.util.Observable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MyModel extends Observable implements IModel {
+
+    private ExecutorService threadPool = Executors.newCachedThreadPool();
+    //TODO - set the private properties from jar and check null and outOfBound
+    private int characterPositionRow = 0; //to get start point
+    private int characterPositionColumn = 0; //to get stars point
+    private int[][] maze; // to build from jar
+    private int maxRow = 0;
+    private int maxCol = 0;
+
+
+    public MyModel() {
+        //Raise the servers
+    }
+
+    public void startServers() {
+    }
+
+    public void stopServers() {
+    }
 
     public void generateMaze(int width, int height){
-        //TODO
+        threadPool.execute(() -> {
+            IMazeGenerator mg = new MyMazeGenerator();
+            Maze MyMaze = mg.generate(height, width);
+            this.maze = MyMaze.getData();
+            this.characterPositionRow = MyMaze.getStartPosition().getRow();
+            this.characterPositionColumn = MyMaze.getStartPosition().getCol();
+
+            try {
+                //TODO - check : is needed ??
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            setChanged();
+            notifyObservers();
+        });
     }
     public void moveCharacter(KeyCode movement){
-        //TODO
+        //TODO - check if the move is no into wall or outOfBound
+        switch (movement) {
+            case UP:
+                if(characterPositionRow>0 && notWall(characterPositionRow-1,characterPositionColumn)){characterPositionRow--;}
+                break;
+            case DOWN:
+                if(characterPositionRow<this.maxRow-1 && notWall(characterPositionRow+1,characterPositionColumn) ){characterPositionRow++;}
+                break;
+            case RIGHT:
+                if(characterPositionColumn<this.maxCol-1 && notWall(characterPositionRow,characterPositionColumn+1) ){characterPositionColumn++;}
+                break;
+            case LEFT:
+                if(characterPositionColumn>0 && notWall(characterPositionRow,characterPositionColumn-1) ){characterPositionColumn--;}
+                break;
+        }
+        setChanged();
+        notifyObservers();
     }
     public int[][] getMaze(){
-        //TODO
-        return null;
+        return maze;
     }
     public int getCharacterPositionRow(){
-        //TODO
-        return 0;
+        return characterPositionRow;
     }
     public int getCharacterPositionColumn(){
-        //TODO
-        return 0;
+        return characterPositionColumn;
+    }
+
+    private boolean notWall(int row,int col){
+        if(maze[row][col]==0){
+            return true;
+        }
+        return false;
     }
 
 }
--- src/ViewModel/MyViewModel.java
+++ src/ViewModel/MyViewModel.java
@@ -1,4 +1,56 @@
 package ViewModel;
 
-public class MyViewModel {
+import Model.IModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.input.KeyCode;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MyViewModel extends Observable implements Observer {
+
+    private IModel model;
+
+    private int characterPositionRowIndex;
+    private int characterPositionColumnIndex;
+    public StringProperty characterPositionRow = new SimpleStringProperty("0"); //For Binding
+    public StringProperty characterPositionColumn = new SimpleStringProperty("0"); //For Binding
+
+    public MyViewModel(IModel model){
+        this.model = model;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o==model){
+            characterPositionRowIndex = model.getCharacterPositionRow();
+            characterPositionRow.set(characterPositionRowIndex + "");
+            characterPositionColumnIndex = model.getCharacterPositionColumn();
+            characterPositionColumn.set(characterPositionColumnIndex + "");
+            setChanged();
+            notifyObservers();
+        }
+    }
+
+    public void generateMaze(int width, int height){
+        model.generateMaze(width, height);
+    }
+
+    public void moveCharacter(KeyCode movement){
+        model.moveCharacter(movement);
+    }
+
+    public int[][] getMaze() {
+        return model.getMaze();
+    }
+
+    public int getCharacterPositionRow() {
+        return characterPositionRowIndex;
+    }
+
+    public int getCharacterPositionColumn() {
+        return characterPositionColumnIndex;
+    }
+
 }
