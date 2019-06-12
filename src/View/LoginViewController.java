9f3b65a56e4e848b4941f836da270c8245714a96
razkl3124 <48918799+razkl3124@users.noreply.github.com>
2019-06-12 16:18:30 +0300

    Best Version 16:18 Wednesday

--- src/View/LoginViewController.java
+++ src/View/LoginViewController.java
@@ -76,8 +76,8 @@ public class LoginViewController extends ControllerAbstract {
         if(s.isEmpty()) return false;
         for(int i = 0; i < s.length(); i++) {
             if(i == 0 && s.charAt(i) == '-') {
-                if(s.length() == 1) return false;
-                else continue;
+                return false;
+
             }
             if(Character.digit(s.charAt(i),radix) < 0) return false;
         }
